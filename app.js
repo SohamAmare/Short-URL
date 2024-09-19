2c5a9bba58000260868095be5e51c7cb49d0853f
SohamAmare <sohamamare07@gmail.com>
2024-09-19 16:55:10 +0530

    Modified 2

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { connectToMongoDB } = require("./connect");
 const path = require('path');
+const cookieParser = require("cookie-parser");
+const {restrictToLoggedinUserOnly, checkAuth} = require("./middleware/auth");
 const URL = require('./models/url');
+
 const urlRoute = require('./routes/url');
 const staticRoute = require('./routes/staticRouter');
+const userRoute = require("./routes/user");
+
 const app = express();
 const PORT = 5000;
 
@@ -15,9 +20,11 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use("/url", urlRoute);
+app.use(cookieParser());
 
-app.use("/", staticRoute);
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
 
 app.get('/url/:shortId', async(req, res) => {
     const shortId = req.params.shortId;
